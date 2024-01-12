01368870382dae09c47f8985c1f2cf58768fa11f
CiarJ <cjones.de117@gmail.com>
2024-01-12 16:08:27 +0100

    Refreshed the API so it works again.

--- src/DisplayIP.jsx
+++ src/DisplayIP.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect } from "react";
+import { DateTime } from "luxon";
 
 const DisplayIP = () => {
     const [ip, setIP] = React.useState("");
     const [location, setLocation] = React.useState("");
     const [country, setCountry] = React.useState("");
     const [flag, setFlag] = React.useState("");
+    const [timezone, setTimezone] = React.useState("");
 
     const fetchIP = async () => {
         const apiKey = import.meta.env.VITE_APP_API_KEY;
 
         try {
-
-            console.log("fetching data");
             let response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`);
             let data = await response.json();
+            console.log(data);
 
             let ip = data.ip;
             let location = data.location.city;
             let country = data.location.country;
 
+            let timezone = data.location.timezone;
+            console.log(timezone);
+
             setIP(ip);
             setLocation(location);
             setCountry(country);
-        
-            console.log("This is the ip address: " + ip);
-            console.log("This is the location: " + location);
-            console.log("This is the country name: " + country);
+            setTimezone(timezone);
+
 
         
             if (country) { 
@@ -63,8 +65,9 @@ const DisplayIP = () => {
     return (
         <div id="infoDiv">
             <div id="ipInfo">
-                <h4>Your IP Address is: {ip}</h4>
-                <h5>Your location is: {location}, {country}</h5>
+                <h4>Your location is: {location}, {country}</h4>
+                <h5>Your IP Address is: {ip}</h5>
+                <h5>The local time is: {}</h5>
             </div>
 
             <div id="flagDiv">
